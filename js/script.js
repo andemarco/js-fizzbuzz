c22f1d9d9ef25ef095dd889ddaa8de2f30a5dcb7
andemarco <andreademarco91@gmail.com>
2020-01-09 14:06:21 +0100

    Seconda parte JS sembra OK

--- js/script.js
+++ js/script.js
@@ -7,8 +7,9 @@
 // che di 5 stampi FizzBuzz.
 
 for (i = 1; i <= 100; i++) {
-  console.log(i);
-  if (i % 3 == 0) {
-    console.log('fizz')
+  if (i % 3 == 0 && i % 5 == 0) {
+    console.log('FizzBuzz')
+  } else if (i % 3 == 0){
+    console.log('Fizz');
   }
 }
