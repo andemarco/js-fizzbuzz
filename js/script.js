4ec5274fd0fe41fe53b6ddac7306f98d1059f712
andemarco <andreademarco91@gmail.com>
2020-01-09 14:08:59 +0100

    JS OK ora da stampare

--- js/script.js
+++ js/script.js
@@ -11,5 +11,9 @@ for (i = 1; i <= 100; i++) {
     console.log('FizzBuzz')
   } else if (i % 3 == 0){
     console.log('Fizz');
+  } else if (i % 5 == 0) {
+    console.log('Buzz')
+  } else {
+    console.log(i)
   }
 }
